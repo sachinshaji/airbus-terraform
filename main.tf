ab3ff13bd153383b0ecaf0462bf92f8d8b4d6017
sachinshaji <51999242+sachinshaji@users.noreply.github.com>
2022-03-26 20:47:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ resource "aws_security_group" "allow_ssh_jenkins" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
 }
 
